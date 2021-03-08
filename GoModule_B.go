e918c048dd75f0278f5c5ff001fef7105583cb33
Lvweikun <Lvweikun@bytedance.com>
2021-03-08 16:26:03 +0800

    first commit

---
+++ GoModule_B.go
@@ -0,0 +1,9 @@
+package GoModule_B
+
+import (
+	"github.com/wklyu/GoModule_C"
+)
+
+func BHello() {
+	GoModule_C.CHello()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wklyu/GoModule_B
+
+go 1.15
+
+require github.com/wklyu/GoModule_C v1.1.0
